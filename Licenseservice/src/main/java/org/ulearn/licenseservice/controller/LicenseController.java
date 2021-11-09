c789608c43340f45a79411c9a6404aff66a4ba82
saikatdhara346@gmail.com <saikatdhara346@gmail.com>
2021-11-09 17:39:00 +0530

    licenses add

--- Licenseservice/src/main/java/org/ulearn/licenseservice/controller/LicenseController.java
+++ Licenseservice/src/main/java/org/ulearn/licenseservice/controller/LicenseController.java
@@ -1,10 +1,31 @@
 package org.ulearn.licenseservice.controller;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.ulearn.licenseservice.entity.LicenseEntity;
+import org.ulearn.licenseservice.servises.LicenseService;
 
 @RestController
 @RequestMapping("/license")
 public class LicenseController {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(LicenseController.class);
+	
+	@Autowired
+	private LicenseService licenseService;
+	
+	
+	@PostMapping("/add")
+	public void addLicense(@RequestBody LicenseEntity licenseEntity ) {
+		
+		LOGGER.info("Inside the LicenseController Add License");
+		
+		licenseService.addLicense(licenseEntity);
+	}
 }
--- Licenseservice/src/main/java/org/ulearn/licenseservice/servises/LicenseService.java
+++ Licenseservice/src/main/java/org/ulearn/licenseservice/servises/LicenseService.java
@@ -1,8 +1,28 @@
 package org.ulearn.licenseservice.servises;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.ulearn.licenseservice.entity.LicenseEntity;
+import org.ulearn.licenseservice.repository.LicenseRepo;
 
 @Service
 public class LicenseService {
+	
+	@Autowired 
+	public LicenseRepo LicenseRepo;
+	
+	public void addLicense( LicenseEntity license ) {
+	
+		try {
+			
+			LicenseEntity licenseAdd = new LicenseEntity();
+			
+			
+			
+		}
+		catch(Exception ex) {
+			ex.getMessage();
+		}
+	}
 
 }
