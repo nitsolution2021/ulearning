fdb88e194a27ae25925bce2564a2e425977af2c2
nitsolution2021 <praveen@nitsolution.in>
2021-11-09 17:30:54 +0530

    add error framework in institute and license

---
+++ Instituteservice/src/main/java/org/ulearn/instituteservice/exception/CustomeException.java
@@ -0,0 +1,27 @@
+package org.ulearn.instituteservice.exception;
+
+public class CustomeException extends RuntimeException {
+	public CustomeException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public CustomeException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+	public CustomeException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public CustomeException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public CustomeException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+}
