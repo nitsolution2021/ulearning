e9fcee2f339f610e45a97a970ccbac799b12c558
soumendoluinit <soumen.dolui@nitsolution.in>
2021-11-09 12:35:50 +0530

    fix marge

--- loginservice/src/main/java/org/ulearn/login/loginservice/entity/LoginEntity.java
+++ loginservice/src/main/java/org/ulearn/login/loginservice/entity/LoginEntity.java
@@ -2,6 +2,8 @@ package org.ulearn.login.loginservice.entity;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -10,6 +12,7 @@ import javax.persistence.Table;
 public class LoginEntity {
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	@Column(name = "UID")
 	private Long uid;
 	@Column(name = "FNAME")
