98e74525411cc0cc00cd041a0625367b62bfcf00
ahadul02 <ahadul@nitsolution.in>
2021-11-09 18:23:21 +0530

    Add Institute Fetch

--- Instituteservice/src/main/java/org/ulearn/instituteservice/repository/InstituteRepo.java
+++ Instituteservice/src/main/java/org/ulearn/instituteservice/repository/InstituteRepo.java
@@ -7,4 +7,5 @@ import org.ulearn.instituteservice.entity.InstituteEntrity;
 
 public interface InstituteRepo extends JpaRepository<InstituteEntrity, Long> {
 	List<InstituteEntrity> findByInstName(String instName);
+	
 }
