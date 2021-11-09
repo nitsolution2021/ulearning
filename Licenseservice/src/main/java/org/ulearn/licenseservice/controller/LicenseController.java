9268b23df16ab7d291a1fda681a2e8784e829e69
nitsolution2021 <praveen@nitsolution.in>
2021-11-09 15:26:11 +0530

    create licenses project

---
+++ Licenseservice/src/main/java/org/ulearn/licenseservice/controller/LicenseController.java
@@ -0,0 +1,10 @@
+package org.ulearn.licenseservice.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/license")
+public class LicenseController {
+
+}
