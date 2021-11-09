9f66842c1ea007b655c8772d505a7afc1717e075
nitsolution2021 <praveen@nitsolution.in>
2021-11-09 12:15:43 +0530

    Create institute service

---
+++ Institutesrvice/src/test/java/org/ulearn/Institutesrvice/InstitutesrviceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ulearn.Institutesrvice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstitutesrviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
