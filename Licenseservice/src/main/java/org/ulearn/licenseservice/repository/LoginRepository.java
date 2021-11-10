462a3d703a92a71c6d5a0f3cec6abdf1e05a3e45
soumendoluinit <soumen.dolui@nitsolution.in>
2021-11-10 19:00:28 +0530

    fix marge

---
+++ Licenseservice/src/main/java/org/ulearn/licenseservice/repository/LoginRepository.java
@@ -0,0 +1,20 @@
+package org.ulearn.licenseservice.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.ulearn.licenseservice.entity.LoginEntity;
+
+
+public interface LoginRepository extends JpaRepository<LoginEntity, Long> {
+
+	Optional<LoginEntity> findByUserName(String userName);
+
+	public Optional<LoginEntity> findByEmail(String email);
+	
+	@Query(value = "select login from LoginEntity login where userName = ?1 and accessToken = ?2")
+	Optional<LoginEntity> findByUserNameAndToken(String userName,String accessToken);
+	
+
+}
