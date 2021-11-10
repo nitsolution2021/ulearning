8f3efb05158b8c8feb3d42f003d88955398e1047
saikatdhara346@gmail.com <saikatdhara346@gmail.com>
2021-11-10 18:08:08 +0530

    push license add,update,list,view on going

--- Licenseservice/src/main/java/org/ulearn/licenseservice/controller/LicenseController.java
+++ Licenseservice/src/main/java/org/ulearn/licenseservice/controller/LicenseController.java
@@ -1,10 +1,16 @@
 package org.ulearn.licenseservice.controller;
 
 
+import java.util.List;
+import java.util.Optional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,5 +34,25 @@ public class LicenseController {
 		LOGGER.info("Inside the LicenseController Add License");
 		
 		return this.licenseService.addLicense(licenseEntity);
+	}
+	
+	@PutMapping("/update/{licenseId}")
+	public GlobalResponse updateLicense(@PathVariable() long licenseId, @RequestBody LicenseEntity licenseForUpdate ) {
+		
+		LOGGER.info("Inside the LicenseController Update License");
+		
+		return this.licenseService.updateLicense(licenseId,licenseForUpdate);
+	}
+	
+	@GetMapping("/list")
+	public List<LicenseEntity> getLicenseList() {
+		
+		return this.licenseService.getAllLicenseList();
+	}
+	
+	@GetMapping("/view/{lcId}")
+	public Optional<LicenseEntity> getLicense(@PathVariable long lcId){
+		
+		return this.licenseService.getLicense(lcId);
 	}
 }
--- Licenseservice/src/main/java/org/ulearn/licenseservice/servises/LicenseService.java
+++ Licenseservice/src/main/java/org/ulearn/licenseservice/servises/LicenseService.java
@@ -1,6 +1,8 @@
 package org.ulearn.licenseservice.servises;
 
 import java.util.Date;
+import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -25,15 +27,15 @@ public class LicenseService {
 				
 				LicenseEntity licenseAdd = new LicenseEntity();
 				
+				licenseAdd.setLcName(license.getLcName());
 				licenseAdd.setLcCreatDate(license.getLcCreatDate());
 				licenseAdd.setLcType(license.getLcType());
 				licenseAdd.setLcStype(license.getLcStype());
 				licenseAdd.setLcValidityType(license.getLcValidityType());
 				licenseAdd.setLcValidityNum(license.getLcValidityNum());
 				licenseAdd.setLcEndDate(license.getLcEndDate());
-				licenseAdd.setHsnId(license.getHsnId());
 				licenseAdd.setLcComment(license.getLcComment());
-				licenseAdd.setLcStatus(license.getLcStatus());
+				licenseAdd.setLcStatus("Complete");
 				licenseAdd.setIsActive(1);
 				licenseAdd.setIsDeleted(0);
 				licenseAdd.setCreatedOn(new Date());
@@ -48,6 +50,87 @@ public class LicenseService {
 		}
 		catch(Exception ex) {
 			throw new CustomeException(ex.getMessage());
+		}
+		
+	}
+
+	public GlobalResponse updateLicense(long licenseId, LicenseEntity licenseForUpdate) {
+		// TODO Auto-generated method stub
+		
+		try {
+			
+			Optional<LicenseEntity> findById = this.LicenseRepo.findById(licenseId);
+			if(findById.isPresent()) {
+				if(findById.get().getLcId().equals(licenseId)) {
+					LicenseEntity licenseUpdate = new LicenseEntity();
+					
+					licenseUpdate.setLcName(licenseForUpdate.getLcName());
+					licenseUpdate.setLcCreatDate(licenseForUpdate.getLcCreatDate());
+					licenseUpdate.setLcType(licenseForUpdate.getLcType());
+					licenseUpdate.setLcStype(licenseForUpdate.getLcStype());
+					licenseUpdate.setLcValidityType(licenseForUpdate.getLcValidityType());
+					licenseUpdate.setLcValidityNum(licenseForUpdate.getLcValidityNum());
+					licenseUpdate.setLcEndDate(licenseForUpdate.getLcEndDate());
+					licenseUpdate.setLcComment(licenseForUpdate.getLcComment());
+					licenseUpdate.setLcStatus(licenseForUpdate.getLcStatus());
+					licenseUpdate.setUpdatedOn(new Date());
+					
+					LicenseEntity save = LicenseRepo.save(licenseUpdate);
+					
+					if(!save.equals(null)) {
+						return new GlobalResponse("Success","Update Successfull");
+					}
+					else {
+						throw new CustomeException("Upade not successfull");
+					}
+				}
+				else {
+					throw new CustomeException("License id not matched");
+				}
+			}
+			else {
+				throw new CustomeException("License not found for this Id"+licenseId);
+			}
+		}
+		catch(Exception ex) {
+			throw new CustomeException(ex.getMessage());
+		}
+	}
+
+	public List<LicenseEntity> getAllLicenseList() {
+		// TODO Auto-generated method stub
+		
+		try {
+			
+			List<LicenseEntity> findAll = LicenseRepo.findAllIsNotDeleted();
+			if(findAll.size()<1) {
+				throw new CustomeException("No license found");
+			}
+			else {
+				return findAll;
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+			throw new CustomeException(e.getMessage());
+		}
+	}
+
+	public Optional<LicenseEntity> getLicense(long lcId) {
+		// TODO Auto-generated method stub
+		
+		try {
+			
+			Optional<LicenseEntity> findById = LicenseRepo.findById(lcId);
+			if(findById.isPresent()) {
+				return findById;
+			}
+			else {
+				throw new CustomeException("No license found for this id");
+			}
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+			throw new CustomeException(e.getMessage());
 		}
 		
 	}
