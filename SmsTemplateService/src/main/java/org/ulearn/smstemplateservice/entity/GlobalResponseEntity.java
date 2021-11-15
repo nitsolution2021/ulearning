b508acb9c35197339b0876adc823476ae8ceb2d2
nitsolution2021 <praveen@nitsolution.in>
2021-11-15 23:47:23 +0530

    push from shantanu

--- SmsTemplateService/src/main/java/org/ulearn/smstemplateservice/entity/GlobalResponseEntity.java
+++ SmsTemplateService/src/main/java/org/ulearn/smstemplateservice/entity/GlobalResponseEntity.java
@@ -3,16 +3,18 @@ package org.ulearn.smstemplateservice.entity;
 public class GlobalResponseEntity {
 	private String reason;
 	private String message;
+	private int statuss;
 
 	public GlobalResponseEntity() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
 
-	public GlobalResponseEntity(String reason, String message) {
+	public GlobalResponseEntity(String reason, String message, int statuss) {
 		super();
 		this.reason = reason;
 		this.message = message;
+		this.statuss = statuss;
 	}
 
 	public String getReason() {
@@ -29,6 +31,14 @@ public class GlobalResponseEntity {
 
 	public void setMessage(String message) {
 		this.message = message;
+	}
+
+	public int getStatuss() {
+		return statuss;
+	}
+
+	public void setStatuss(int statuss) {
+		this.statuss = statuss;
 	}
 
 }
