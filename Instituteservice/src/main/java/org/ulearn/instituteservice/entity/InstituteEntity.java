a63baca529ef7c6f13b281babe2591b8757eaab3
ahadul02 <ahadul@nitsolution.in>
2021-11-18 19:01:05 +0530

    Institute Intregate

--- Instituteservice/src/main/java/org/ulearn/instituteservice/entity/InstituteEntity.java
+++ Instituteservice/src/main/java/org/ulearn/instituteservice/entity/InstituteEntity.java
@@ -13,6 +13,8 @@ import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonIgnoreProperties({"hibernateLazyInitializer"})
