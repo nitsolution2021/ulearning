7e2e669c2b85477480245cd67ff0068957c01d8e
NITDEBANJAN <debanjan@nitsolution.in>
2021-11-23 19:13:11 +0530

    Update

--- packageservice/src/main/java/org/ulearn/packageservice/PackageserviceApplication.java
+++ packageservice/src/main/java/org/ulearn/packageservice/PackageserviceApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.ulearn.packageservice.config.YMLConfig;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
