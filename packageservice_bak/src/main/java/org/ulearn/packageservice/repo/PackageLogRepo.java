c9caa0720796dd3d9c559edd751c0eccc0645718
ahadul02 <ahadul@nitsolution.in>
2021-11-26 16:07:48 +0530

    Package Changes Added

--- packageservice_bak/src/main/java/org/ulearn/packageservice/repo/PackageLogRepo.java
+++
@@ -1,9 +0,0 @@
-package org.ulearn.packageservice.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.ulearn.packageservice.entity.PackageLogEntity;
-
-public interface PackageLogRepo extends JpaRepository<PackageLogEntity, Long>{
-
-	
-}
