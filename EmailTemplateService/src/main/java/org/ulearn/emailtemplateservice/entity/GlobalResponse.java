90e766fa3c569e9d5e105cb83ee1d8ead382be3c
nitsolution2021 <praveen@nitsolution.in>
2021-11-27 13:37:25 +0530

    geta page wise data

--- EmailTemplateService/src/main/java/org/ulearn/emailtemplateservice/entity/GlobalResponse.java
+++ EmailTemplateService/src/main/java/org/ulearn/emailtemplateservice/entity/GlobalResponse.java
@@ -3,36 +3,39 @@ package org.ulearn.emailtemplateservice.entity;
 public class GlobalResponse {
 	private String reason;
 	private String message;
-
+	private int status;
+	public String getReason() {
+		return reason;
+	}
+	public void setReason(String reason) {
+		this.reason = reason;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public int getStatus() {
+		return status;
+	}
+	public void setStatus(int status) {
+		this.status = status;
+	}
+	@Override
+	public String toString() {
+		return "GlobalResponse [reason=" + reason + ", message=" + message + ", status=" + status + "]";
+	}
+	public GlobalResponse(String reason, String message, int status) {
+		super();
+		this.reason = reason;
+		this.message = message;
+		this.status = status;
+	}
 	public GlobalResponse() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
 
-	public GlobalResponse(String reason, String message) {
-		super();
-		this.reason = reason;
-		this.message = message;
-	}
-
-	@Override
-	public String toString() {
-		return "GlobalResponse [reason=" + reason + ", message=" + message + "]";
-	}
-
-	public String getReason() {
-		return reason;
-	}
-
-	public void setReason(String reason) {
-		this.reason = reason;
-	}
-
-	public String getMessage() {
-		return message;
-	}
-
-	public void setMessage(String message) {
-		this.message = message;
-	}
+	
 }
