1660e7b361fa947541a4b498402b94f2150da050
NITDEBANJAN <debanjan@nitsolution.in>
2021-11-29 12:56:58 +0530

    updated

--- packageservice/packageservice/src/main/java/org/ulearn/packageservice/repo/InstituteRepo.java
+++ packageservice/packageservice/src/main/java/org/ulearn/packageservice/repo/InstituteRepo.java
@@ -1,9 +1,15 @@
 package org.ulearn.packageservice.repo;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.ulearn.packageservice.entity.InstituteEntity;
 @Repository
 public interface InstituteRepo extends JpaRepository<InstituteEntity, Long>{
+
+	@Query(value="SELECT instObj FROM InstituteEntity instObj")
+	Page<InstituteEntity> findByAllInst(Pageable pagingSort);
 
 }
