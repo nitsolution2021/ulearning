e0f1677cd68f375fb44e9a7054fe1cf394fa6a29
nitsolution2021 <praveen@nitsolution.in>
2021-12-08 13:40:14 +0530

    delete package service

--- packageservice/src/main/java/org/ulearn/packageservice/repo/LicenseRepo.java
+++
@@ -1,8 +0,0 @@
-package org.ulearn.packageservice.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.ulearn.packageservice.entity.LicenseEntity;
-
-public interface LicenseRepo extends JpaRepository<LicenseEntity, Long>{
-
-}
